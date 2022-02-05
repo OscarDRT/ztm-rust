f8bd157d31ddc13ef62b1ff900dad8629be921ab
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-04 22:15:13 -0500

    feat: solution activity a2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -5,7 +5,17 @@
 //
 // Notes:
 // * Use a function to add two numbers together
+fn sum(a: i32, b: i32) -> i32 {
+  a + b
+}
+
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
+fn print_result(result: i32) {
+  println!("{:?}", result)
+}
 
-fn main() {}
+fn main() {
+  let result = sum(2, 3);
+  print_result(result)
+}
