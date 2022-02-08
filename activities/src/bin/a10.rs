fde49094540384b1c84cf1439b87deb6b519a2a0
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-08 18:52:17 -0500

    feat: solution activity a10

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -5,11 +5,23 @@
 // * Print "its small" if a variable is <= 100
 //
 // Notes:
-// * Use a boolean variable set to the result of
-//   an if..else expression to store whether the value
-//   is > 100 or <= 100
+
 // * Use a function to print the messages
 // * Use a match expression to determine which message
 //   to print
+fn print_result(n: bool) {
+  match n {
+    true => println!("its big"),
+    false => println!("its small"),
+  };
+}
 
-fn main() {}
+
+fn main() {
+  // * Use a boolean variable set to the result of
+  //   an if..else expression to store whether the value
+  //   is > 100 or <= 100
+  let number = 101;
+  let is_big = number > 100;
+  print_result(is_big)
+}
