06d13bafd4074e64c0147f3ee195eeaf12e12ac7
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-08 18:36:51 -0500

    feat: solution activity a9

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -5,8 +5,25 @@
 //   greater than 5, less than 5, or equal to 5
 //
 // Notes:
-// * Use a function that returns a tuple
-// * Destructure the return value into two variables
-// * Use an if..else if..else block to determine what to print
 
-fn main() {}
+
+
+// * Use a function that returns a tuple
+fn get_coords()-> (i32, i32){
+  (5, 6)
+}
+
+fn main() {
+  // * Destructure the return value into two variables
+  // * Use an if..else if..else block to determine what to print
+  let (x, y) = get_coords();
+
+  if y > 5 {
+    println!("y-value greater than 5");
+  } else if y < 5 {
+    println!("y-value less than 5");
+  }else {
+    println!("equal to 5");
+  }
+  println!("{:?}", x);
+}
