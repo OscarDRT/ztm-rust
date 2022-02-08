ed793a19c879f6c6a6716f43c543d73ae43c0775
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-08 15:40:02 -0500

    feat: solution activity a7

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -4,10 +4,29 @@
 // * Prints the name of a color to the terminal
 //
 // Notes:
+
+
+
 // * Use an enum with color names as variants
+enum Colors {
+    BLue,
+    Red,
+    Black,
+}
+
 // * Use a function to print the color name
 // * The function must use the enum as a parameter
 // * Use a match expression to determine which color
 //   name to print
+fn print_color(my_comlor: Colors) {
+  match my_comlor {
+    Colors::BLue => println!("Blue"),
+    Colors::Red => println!("Red"),
+    Colors::Black => println!("Black"),
+  }
+}
 
-fn main() {}
+fn main() {
+  let color = Colors::BLue;
+  print_color(color);
+}
