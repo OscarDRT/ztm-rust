c257d643c59b316607f7c9807d463dcec987da61
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-10 13:36:29 -0500

    feat: solution activity a11

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -6,7 +6,24 @@
 // Notes:
 // * Use a struct for the grocery item
 // * Use two i32 fields for the quantity and id number
+struct Item {
+  quantity: i32,
+  id: i32,
+}
 // * Create a function to display the quantity, with the struct as a parameter
+fn display_quantity(i: &Item) {
+  println!("Quantity: {:?}", i.quantity);
+}
 // * Create a function to display the id number, with the struct as a parameter
+fn display_id(i: &Item) {
+  println!("Id: {:?}", i.id);
+}
 
-fn main() {}
+fn main() {
+  let item = Item {
+    quantity: 32,
+    id: 1234,
+  };
+  display_quantity(&item);
+  display_id(&item);
+}
