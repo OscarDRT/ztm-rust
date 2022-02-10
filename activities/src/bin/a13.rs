f1cdfc08fcb87bd55cb8371b8fcebfff7531ca6c
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-10 18:22:33 -0500

    feat: solution activity a13

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -5,9 +5,21 @@
 // * Print the total number of elements in a vector
 //
 // Notes:
-// * Use a vector to store 4 numbers
-// * Iterate through the vector using a for..in loop
-// * Determine whether to print the number or print "thirty" inside the loop
-// * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+
+
+fn main() {
+  // * Use a vector to store 4 numbers
+  let vector = vec![10, 20, 30, 40];
+  
+  // * Iterate through the vector using a for..in loop
+  // * Determine whether to print the number or print "thirty" inside the loop
+  for elem in &vector {
+    match elem {
+        30 => println!("thirty"),
+        _ => println!("{}", elem)
+    }
+  }
+  // * Use the .len() function to print the number of elements in a vector
+  println!("Number of elements: {}", vector.len())
+}
