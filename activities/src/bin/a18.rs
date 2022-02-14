4d8ef08f5ab54742234249f8825edbd8b04349d0
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-13 23:24:19 -0500

    feat: solution activity a18

--- activities/src/bin/a18.rs
+++ activities/src/bin/a18.rs
@@ -7,8 +7,22 @@
 //
 // Notes:
 // * Use a struct to store at least the age of a customer
+struct Custumer {
+  age: i32
+}
 // * Use a function to determine if a customer can make a restricted purchase
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
+fn try_purchase(custumer: &Custumer) -> Result<(), String> {
+  if custumer.age >= 21 {
+    Ok(())
+  } else {
+      Err("Under 21".to_owned())
+  }
+}
 
-fn main() {}
+fn main() {
+  let oscar = Custumer {age: 28};
+  let purchased = try_purchase(&oscar);
+  println!("{:?}", purchased)
+}
