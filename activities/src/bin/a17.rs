7ea5717b0a3a1ee67ec594bf2778cb2e99d07bf4
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-13 22:40:24 -0500

    feat: solution activity a17

--- activities/src/bin/a17.rs
+++ activities/src/bin/a17.rs
@@ -4,12 +4,16 @@
 // * Print a string in lowercase and uppercase
 //
 // Notes:
-// * Utilize standard library functionality to
-//   transform the string to lowercase and uppercase
 // * Use 'rustup doc' in a terminal to open the standard library docs
 // * Navigate to the API documentation section
 // * Search for functionality to transform a string (or str)
 //   to uppercase and lowercase
 //   * Try searching for: to_uppercase, to_lowercase
 
-fn main() {}
+fn main() {
+  let text = "Oscar DAVID riano TaPiAs";
+  // * Utilize standard library functionality to
+  // transform the string to lowercase and uppercase
+  println!("{:?}", text.to_uppercase());
+  println!("{:?}", text.to_lowercase())
+}
