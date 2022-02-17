79eb7fab4f465bb090feb6f09bd27b3d963c049d
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-16 21:20:46 -0500

    feat: solution activity a24

--- activities/src/bin/a24.rs
+++ activities/src/bin/a24.rs
@@ -10,4 +10,14 @@
 
 fn main() {
     let data = vec![1, 2, 3, 4, 5];
+
+    let data_copie: Vec<_> = data
+        .iter()
+        .map(|num| num *3)
+        .filter(|num| num > &10)
+        .collect();
+
+    for elem in data_copie {
+        println!("{:?}", elem)  
+    }
 }
