13488113eb88c56025857ae3a9463df033089b06
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-16 23:00:14 -0500

    feat: solution activity a22

--- activities/src/bin/a22.rs
+++ activities/src/bin/a22.rs
@@ -11,6 +11,7 @@
 //     determine how the they should operate.
 
 /// Ensures n is >= lower and <= upper.
+/// lower - n - upper
 fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
     if n < lower {
         lower
@@ -31,4 +32,59 @@ fn concat(first: &str, second: &str) -> String {
     format!("{} {}", first, second)
 }
 
-fn main() {}
+fn main() {
+    let a = clamp(10, 100, 1000);
+    println!("{:?}", a);
+
+    let b = clamp(10, 10, 10);
+    println!("{:?}", b);
+
+    let c = clamp(100, 10, 10);
+    println!("{:?}", c);
+
+    let d = div(10, 2);
+    println!("{:?}", d);
+
+    let e = concat("first", "second");
+    println!("{:?}", e);
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn clamp_lower() {
+        let result = clamp(10, 100, 1000);
+        let expected = 100;
+        assert_eq!(result, expected, "Should be 100");
+    }
+
+    #[test]
+    fn check_clamp() {
+        let result = clamp(10, 10, 10);
+        let expected = 10;
+        assert_eq!(result, expected, "Should be 10");
+    }
+
+    #[test]
+    fn clamp_upper() {
+        let result = clamp(100, 10, 10);
+        let expected = 10;
+        assert_eq!(result, expected, "Should be 100");
+    }
+
+    #[test]
+    fn check_div() {
+        let result = div(10, 2);
+        let expected = Some(5);
+        assert_eq!(result, expected, "Should be Some(5)");
+    }
+
+    #[test]
+    fn check_concat() {
+        let result = concat("first", "second");
+        let expected = "first second";
+        assert_eq!(result, expected, "Should be first second");
+    }
+}
