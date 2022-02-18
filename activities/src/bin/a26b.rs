59059cec58eeb91f4cc56fdc934184a2e3ee4a4e
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-18 16:09:22 -0500

    feat: solution activity a26b

--- activities/src/bin/a26b.rs
+++ activities/src/bin/a26b.rs
@@ -15,40 +15,10 @@
 // * After moving the functions into modules, try running
 //   `cargo check --bin a26b` to get a listing of required code changes
 
-mod msg {
-    pub fn trim(msg: &str) -> &str {
-        msg.trim()
-    }
-    
-    pub fn capitalize(msg: &str) -> std::borrow::Cow<'_, str> {
-        if let Some(letter) = msg.get(0..1) {
-            format!("{}{}", letter.to_uppercase(), &msg[1..msg.len()]).into()
-        } else {
-            msg.into()
-        }
-    }
-    
-    pub fn exciting(msg: &str) -> String {
-        format!("{}!", msg)
-    }
-
-}
-
-mod math {
-    pub fn add(lhs: isize, rhs: isize) -> isize {
-        lhs + rhs
-    }
-    pub fn sub(lhs: isize, rhs: isize) -> isize {
-        lhs - rhs
-    }
-    pub fn mul(lhs: isize, rhs: isize) -> isize {
-        lhs * rhs
-    }
-}
-
 
 
 fn main() {
+    use activity::math;
     // Part 1: math functions
     let result = {
         let two_plus_two = math::add(2, 2);
@@ -61,7 +31,8 @@ fn main() {
     println!("(2 + 2 - 1) * 3 = {}", result);
 
     {
-        use msg::{trim, capitalize, exciting};
+
+        use activity::msg::{trim, capitalize, exciting};
         // Part 2: string functions
         let hello = {
             let msg = "hello ";
