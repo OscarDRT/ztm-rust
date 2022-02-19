f686fb74636ddebb11ffbe98bbc37da1f1e479e5
OscarDRT <oscarnetworkingpro@gmail.com>
2022-02-19 16:22:01 -0500

    feat: solution activity a20

--- activities/src/bin/a20.rs
+++ activities/src/bin/a20.rs
@@ -1,3 +1,4 @@
+use std::io::*;
 // Topic: User input
 //
 // Requirements:
@@ -17,10 +18,53 @@
 //
 // Notes:
 // * Use an enum to store the possible power states
+enum PowerState {
+  Off,
+  Sleep,
+  Reboot,
+  Shutdown,
+  Hibernate
+}
+
+// * Use a match expression to convert the user input into the power state enum
+impl PowerState {
+  fn new(state: &str) -> Option<PowerState> {
+    match state.to_lowercase().trim() {
+      "off" => Some(PowerState::Off),
+      "sleep" => Some(PowerState::Sleep),
+      "reboot" => Some(PowerState::Reboot),
+      "shutdown" => Some(PowerState::Shutdown),
+      "hibernate" => Some(PowerState::Hibernate),
+      _ => None
+    }
+  }
+}
+
 // * Use a function with a match expression to print out the power messages
 //   * The function should accept the enum as an input
-// * Use a match expression to convert the user input into the power state enum
+fn print_action(state: PowerState) {
+  use PowerState::*;
+  match state {
+    Off => println!("-> Off <-"),
+    Sleep => println!("-> Sleep <-"),
+    Reboot => println!("-> Reboot <-"),
+    Shutdown => println!("-> Shutdown <-"),
+    Hibernate => println!("-> Hibernate <-"),
+  }
+}
+
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+fn main() {
+  let mut buffer = String::new();
+  let user_input = stdin().read_line(&mut buffer);
+  if user_input.is_ok() {
+    match PowerState::new(&buffer) {
+      Some(state) => print_action(state),
+      None => println!("Invalid statate")
+    }
+  } else {
+    println!("Input Error")
+  }
+}
